0797e6ca6499f2dc96b241d1139551efa3993be0
divyac3 <divyacr036@gmail.com>
2025-09-03 20:29:57 +0530

    Front Page Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
-import { Button } from "@/components/ui/button";
+import BgGradient from "@/components/common/bg-gradient";
+import CTASection from "@/components/home/cta-section";
+import DemoSection from "@/components/home/demo-section";
+import HeroSection from "@/components/home/hero-section";
+import HowItWorksSection from "@/components/home/how-it-works-section";
+import PricingSection from "@/components/home/pricing-section";
 
 
 export default function Home() {
   return (
-    <div className="">
-      
-     <Button variant='outline'>Shadcn Button</Button>
+    <div className="relative w-full ">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection /> 
+        <DemoSection />
+        <HowItWorksSection />
+        <PricingSection />
+        <CTASection/>
+      </div>
+
+    
     </div>
   );
 }
